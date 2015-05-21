b85f6bcd5d4f4e14e4ea9a91f998fd84c3c3998c
Jack Slinger <jackslinger10@gmail.com>
2015-05-21 14:14:33 +0100

    Changed the link from dbpedia to wikipedia.

--- server.js
+++ server.js
@@ -667,15 +667,15 @@ var app = protocol.createServer(function (req, res) {
                     var lon = venue.location.lng;
 
                     //Create the dbpedia sparqle query using the latidute and longidute
-                    var path = "/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=PREFIX+geo%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2003%2F01%2Fgeo%2Fwgs84_pos%23%3E%0D%0ASELECT+%3Fsubject+%3Flabel+%3Flat+%3Flong+WHERE+%7B%0D%0A%3Fsubject+geo%3Alat+%3Flat.%0D%0A%3Fsubject+geo%3Along+%3Flong.%0D%0A%3Fsubject+rdfs%3Alabel+%3Flabel.%0D%0AFILTER%28%0D%0Axsd%3Adouble%28%3Flat%29+-+xsd%3Adouble%28%22";
+                    var path = "/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=PREFIX+geo%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2003%2F01%2Fgeo%2Fwgs84_pos%23%3E%0D%0ASELECT+%3Fsubject+%3Flabel+%3Flat+%3Flong+%3Fpageid+WHERE+%7B%0D%0A%3Fsubject+geo%3Alat+%3Flat.%0D%0A%3Fsubject+geo%3Along+%3Flong.%0D%0A%3Fsubject+rdfs%3Alabel+%3Flabel.%0D%0A%3Fsubject+dbpedia-owl%3AwikiPageID+%3Fpageid.%0D%0AFILTER%28%0D%0Axsd%3Adouble%28%3Flat%29+-+xsd%3Adouble%28%22";
                     path = path + lat;
-                    path = path + "%22%5E%5Exsd%3Afloat%29+%3C%3D+0.5+%26%26%0D%0Axsd%3Adouble%28%22";
+                    path = path + "%22%5E%5Exsd%3Afloat%29+%3C%3D+0.1+%26%26%0D%0Axsd%3Adouble%28%22";
                     path = path + lat;
-                    path = path + "%22%5E%5Exsd%3Afloat%29+-+xsd%3Adouble%28%3Flat%29+%3C%3D+0.5+%26%26%0D%0Axsd%3Adouble%28%3Flong%29+-+xsd%3Adouble%28%22";
+                    path = path + "%22%5E%5Exsd%3Afloat%29+-+xsd%3Adouble%28%3Flat%29+%3C%3D+0.1+%26%26%0D%0Axsd%3Adouble%28%3Flong%29+-+xsd%3Adouble%28%22";
                     path = path + lon;
-                    path = path + "%22%5E%5Exsd%3Afloat%29+%3C%3D+0.5+%26%26%0D%0Axsd%3Adouble%28%22";
+                    path = path + "%22%5E%5Exsd%3Afloat%29+%3C%3D+0.1+%26%26%0D%0Axsd%3Adouble%28%22";
                     path = path + lon;
-                    path = path + "%22%5E%5Exsd%3Afloat%29+-+xsd%3Adouble%28%3Flong%29+%3C%3D+0.5+%26%26%0D%0Alang%28%3Flabel%29+%3D+%22en%22%0D%0A%29.%0D%0A%7D+LIMIT+20&format=application%2Fsparql-results%2Bjson&timeout=30000&debug=on";
+                    path = path + "%22%5E%5Exsd%3Afloat%29+-+xsd%3Adouble%28%3Flong%29+%3C%3D+0.1+%26%26%0D%0Alang%28%3Flabel%29+%3D+%22en%22%0D%0A%29.%0D%0A%7D+LIMIT+20&format=application%2Fsparql-results%2Bjson&timeout=30000&debug=on";
                     
                     //Set the options for the call to dbpedia
                     var options = {
@@ -708,7 +708,8 @@ var app = protocol.createServer(function (req, res) {
                             result.label = binding.label.value;
                             result.lat = binding.lat.value;
                             result.lon = binding.long.value;
-                            result.link = binding.subject.value;
+                            var link = "http://en.wikipedia.org/?curid=" + binding.pageid.value;
+                            result.link = link;
 
                             results[index] = result;
                         }
